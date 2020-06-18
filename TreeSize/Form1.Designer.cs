174264a9eb7044f97a48deb429e706de7bd8941b
Rocket47 <romtswork@gmail.com>
2020-06-18 23:49:16 +0300

    Initial version of the project

---
+++ TreeSize/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace TreeSize
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.listview = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.column_count = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.treeView1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.listview);
+            this.splitContainer1.Size = new System.Drawing.Size(800, 450);
+            this.splitContainer1.SplitterDistance = 266;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // treeView1
+            // 
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeView1.Location = new System.Drawing.Point(0, 0);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(266, 450);
+            this.treeView1.TabIndex = 0;
+            // 
+            // listview
+            // 
+            this.listview.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.column_count});
+            this.listview.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listview.HideSelection = false;
+            this.listview.Location = new System.Drawing.Point(0, 0);
+            this.listview.Name = "listview";
+            this.listview.Size = new System.Drawing.Size(530, 450);
+            this.listview.TabIndex = 0;
+            this.listview.UseCompatibleStateImageBehavior = false;
+            this.listview.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Name";
+            this.columnHeader1.Width = 119;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Type";
+            this.columnHeader2.Width = 104;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Last Modified";
+            this.columnHeader3.Width = 120;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Size";
+            this.columnHeader4.Width = 83;
+            // 
+            // column_count
+            // 
+            this.column_count.Text = "Count";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.ListView listview;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader column_count;
+    }
+}
+
---
+++ TreeSize/Form1.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TreeSize
+{    
+    public partial class Form1 : Form
+    {
+        ListViewItem item;
+        List<DirectoryInfo> saveList = new List<DirectoryInfo>();
+        public delegate void MethodContainer();
+        public Form1()
+        {
+            InitializeComponent();
+            PopulateTreeView();            
+            this.treeView1.NodeMouseClick +=
+            new TreeNodeMouseClickEventHandler(this.treeView1_NodeMouseClick);
+            
+        }
+
+        public void UpdateSize()
+        {
+
+        }
+
+        private void PopulateTreeView()
+        {
+            TreeNode rootNode;
+            DriveInfo[] allDrives = DriveInfo.GetDrives();
+            foreach (DriveInfo driveInfo in allDrives)
+            {
+                DirectoryInfo info = new DirectoryInfo(@driveInfo.Name);
+                if (info.Exists)
+                {
+                    rootNode = new TreeNode(info.Name);
+                    rootNode.Tag = info;
+                    GetDirectories(info.GetDirectories(), rootNode);
+                    treeView1.Nodes.Add(rootNode);
+                }
+            }
+        }
+
+        private void GetDirectories(DirectoryInfo[] subDirs,
+            TreeNode nodeToAddTo)
+        {
+            TreeNode aNode;
+            foreach (DirectoryInfo subDir in subDirs)
+            {
+                try
+                {
+                    subDir.GetFiles();
+                }
+                catch (UnauthorizedAccessException ex)
+                {
+                    continue;
+                }
+                aNode = new TreeNode(subDir.Name, 0, 0);
+                aNode.Tag = subDir;
+                nodeToAddTo.Nodes.Add(aNode);
+            }
+        }
+
+        void treeView1_NodeMouseClick(object sender,
+    TreeNodeMouseClickEventArgs e)
+        {            
+            TreeNode newSelected = e.Node;
+            listview.Items.Clear();
+            DirectoryInfo nodeDirInfo = (DirectoryInfo)newSelected.Tag;
+            ListViewItem.ListViewSubItem[] subItems = new ListViewItem.ListViewSubItem[] { };
+            ListViewItem item = null;
+            FillDirectory(nodeDirInfo);                   
+            //FillFiles(nodeDirInfo);            
+        }
+
+        public long DirSize(DirectoryInfo d)
+        {
+            long size = 0;
+            try
+            {
+                FileInfo[] fis = d.GetFiles();
+                if (fis.Length != 0)
+                {
+                    foreach (FileInfo fi in fis)
+                    {
+                        //listview.Items[index].SubItems[3].Text = size.ToString();
+                        size += fi.Length;
+                    }
+                }
+                DirectoryInfo[] dis = d.GetDirectories();
+                foreach (DirectoryInfo di in dis)
+                {
+                    listview.Items[0].SubItems[3].Text = size.ToString();
+                    size += DirSize(di);                    
+                }
+                return size;
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                return 0;
+            }
+
+        }
+
+        public void FillDirectory(DirectoryInfo nodeDirInfo)
+        {
+            foreach (DirectoryInfo dir in nodeDirInfo.GetDirectories())
+            {                
+                try
+                {                    
+                    Thread thread = new Thread(() =>
+                    {
+                        Invoke((Action)(() => SetInfoDirectory(dir)));                                           
+                    });                    
+                    thread.Start();
+                    saveList.Add(dir);
+                }
+                catch (UnauthorizedAccessException ex)
+                {
+                    continue;
+                }
+            }            
+        }
+
+        public void UpdateSize(DirectoryInfo nodeDirInfo)
+        {
+            foreach (DirectoryInfo dir in saveList)
+            {                
+                try
+                {
+                    Thread thread = new Thread(() =>
+                    {
+                        Invoke((Action)(() => DirSize(dir)));
+                    });
+                    thread.Start();
+                }
+                catch (UnauthorizedAccessException ex)
+                {
+                    continue;
+                }
+            }
+        }
+
+
+        public void SetInfoDirectory(DirectoryInfo dir)
+        {
+            int index = 0;
+            item = new ListViewItem(dir.Name, 0);
+            item.SubItems.Add("Directory");
+            item.SubItems.Add(dir.LastAccessTime.ToShortDateString());          
+           // ListViewItem.ListViewSubItem[] subItems = new ListViewItem.ListViewSubItem[]
+           //{ new ListViewItem.ListViewSubItem(item, "Directory"),
+           //  new ListViewItem.ListViewSubItem(item,
+           //      dir.LastAccessTime.ToShortDateString()),
+           //  new ListViewItem.ListViewSubItem(item, DirSize(dir).ToString()),
+           //  new ListViewItem.ListViewSubItem(item, GetCountDirectories(dir).ToString())};
+            //item.SubItems.AddRange(subItems);            
+            listview.Items.Add(item);
+            index = listview.Items.Count - 1;                       
+        }               
+
+        public int GetCountDirectories(DirectoryInfo dir)
+        {
+            int countDirectories;
+            try
+            {
+                countDirectories = Directory.EnumerateDirectories(dir.FullName, "*",
+                     SearchOption.AllDirectories).Count();
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                return 0;
+            }
+
+            return countDirectories;
+        }
+
+        public void FillFiles(DirectoryInfo nodeDirInfo)
+        {
+            foreach (FileInfo file in nodeDirInfo.GetFiles())
+            {
+                ListViewItem item = new ListViewItem(file.Name, 1);
+                ListViewItem.ListViewSubItem[] subItems = new ListViewItem.ListViewSubItem[]
+                    { new ListViewItem.ListViewSubItem(item, "File"),
+             new ListViewItem.ListViewSubItem(item,
+                file.LastAccessTime.ToShortDateString()), new ListViewItem.ListViewSubItem(item,
+                file.Length.ToString()) };
+
+                item.SubItems.AddRange(subItems);
+                listview.Items.Add(item);
+            }
+        }
+    }
+}
+
